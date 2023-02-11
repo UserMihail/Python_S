7772ad38dd6f68e3f09e7f60529f4960a00de30c
Mihail <horunzhy.m@yandex.ru>
2023-02-11 22:36:38 +0300

    4.1

--- Lesson_1/4.1.py
+++ Lesson_1/4.1.py
@@ -7,9 +7,21 @@
 # a a a b c a a d c d d
 # a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 
-chars = input().split()
-dict_chars = {}.fromkeys(chars, 0)
+# chars = input().split()
+# dict_chars = {}.fromkeys(chars, 0)
 
-for i in chars:
-    print(f"{i}_{dict_chars[i]}" if dict_chars[i] else i, end=" ")
-    dict_chars[i] += 1
+# for i in chars:
+#     print(f"{i}_{dict_chars[i]}" if dict_chars[i] else i, end=" ")
+#     dict_chars[i] += 1
+
+
+# ILI
+
+string = input().split()
+
+for i in range(len(string)):
+    count = 1
+    for j in range(i + 1, len(string)):
+        if string[i] == string[j]:
+            string[j] += "_" + str(count)
+            count += 1
